ad1cc99b548e9a43ae86099af2b00a41aad048ce
Alex Mihov <alexmihov.as@gmail.com>
2025-01-21 14:04:34 +0100

    snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const classeStudente = students.find((classe) => {
+  return classe.name === 'Marco Lanci';
+}).class;
+
+console.log(classeStudente);
+
+
